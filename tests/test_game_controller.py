b59f1d313c19c5f473b075a898c0e08c960504e4
David Lee <medavidli@gmail.com>
2023-09-21 20:19:32 +0000

    add test case

--- tests/test_game_controller.py
+++ tests/test_game_controller.py
@@ -7,14 +7,15 @@ class TestGameController(TestCase):
     def test_init(self):
         testobj = GameController()
         assert testobj.status != None
-"""
+
+    """
     def test_create_character_updates_status(self):
         testobj = GameController()
         arbitrary_name = "ARBITRARY"
         testobj.create_character(arbitrary_name)
         self.assertEqual(arbitrary_name, testobj.status.character_name)
         self.assertIsNotNone(testobj.character)
-"""
+    """
     def test_start_game_creates_map_and_enters_char(self):
         testobj = GameController()
         arbitrary_name = "ARBITRARY"
