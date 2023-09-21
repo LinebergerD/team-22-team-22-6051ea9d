be1ed9f46e5cbbf431a14beaf78c9336b823ada2
yk <nc28078@outlook.com>
2023-09-21 15:50:29 +0000

    fixed failed build

--- tests/test_position.py
+++ tests/test_position.py
@@ -6,4 +6,20 @@ class TestPositionInitWithCoordinates(TestCase):
         xCoordinates = 1
         yCoordinates = 5
         testobj = Position(xCoordinates, yCoordinates)
-        self.assertIsNotNone(testobj)
+        self.assertEqual(xCoordinates, testobj.x)
+        self.assertEqual(yCoordinates, testobj.y)
+
+    def test_equal(self):
+        xCoordinates = 1
+        yCoordinates = 5
+        testobj = Position(xCoordinates, yCoordinates)
+        testobj2 = Position(xCoordinates, yCoordinates)
+        self.assertTrue(testobj == testobj2)
+
+    def test_notEqual(self):
+        xCoordinates = 1
+        yCoordinates = 5
+        testobj = Position(xCoordinates, yCoordinates)
+        testobj2 = Position(xCoordinates, yCoordinates + 1)
+        self.assertTrue(testobj != testobj2)
+        
